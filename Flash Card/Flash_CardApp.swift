6b3bdc1e7857cf8f7f23e7c50e98e4b14dd294a5
Septian Nugraha <septiannugraha341@gmail.com>
2024-11-19 22:00:07 +0700

    Initial Commit

---
+++ Flash Card/Flash_CardApp.swift
@@ -0,0 +1,20 @@
+//
+//  Flash_CardApp.swift
+//  Flash Card
+//
+//  Created by Muhamad Septian Nugraha on 19/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Flash_CardApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
